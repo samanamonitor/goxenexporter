fdbac6465d382e7fd4f4d5ca1a2b2fae5ff15150
Fabian Baena <fabian.baena@samanagroup.com>
2025-03-13 15:07:13 -0400

    x

--- src/xentest.go
+++ src/xentest.go
@@ -60,6 +60,7 @@ func main() {
 	}
 	log.Print("api version: ", session.APIVersion)
 	log.Print("xapi rpm version: ", session.XAPIVersion)
+	log.Printf("%v\n", session)
 
 	if err := session.Logout(); err != nil {
 		log.Print(err)
